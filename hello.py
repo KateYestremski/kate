b3c89fde12662b6a792a46c8fe30e0223d7d197f
KateYestremski <kateyes566@gmail.com>
2024-07-25 09:28:15 -0400

    fmcc work

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("hello world from python\n\n")
+print("i will show you some data")
+myname = "kate"
+age = 19
+eyes = "brown"
+
+print(f"hello, {myname}. you are {age} years old. you have {eyes} eyes.")
